932255312c27c2bb3886593146effc5057f71f87
Dimitri <d.habruk@gmail.com>
2023-01-20 11:53:21 +0100

    done

---
+++ ironHackers.js
@@ -0,0 +1,11 @@
+import chalk from 'chalk'
+
+const name1 = 'Donald'
+const name2 = 'Lea'
+const name3 = 'Daniel'
+const name4 = 'John'
+
+console.log(chalk.blue(name1))
+console.log(chalk.red(name2))
+console.log(chalk.green(name3))
+console.log(chalk.yellow(name4))
